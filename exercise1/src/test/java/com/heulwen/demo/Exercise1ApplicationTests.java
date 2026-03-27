f534d7435c67a6ebe9293121a3cbab6e3fba900c
nguyendzaii16112004@gmail.com <nguyendzaii16112004@gmail.com>
2026-03-27 09:44:47 +0700

    Initial commit

---
+++ exercise1/src/test/java/com/heulwen/demo/Exercise1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heulwen.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
