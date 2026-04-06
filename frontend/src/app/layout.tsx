675247655fcc25ec4b770e765131f6da5d0fff50
Nguyen Chau <nguyendzaii16112004@gmail.com>
2026-04-06 11:36:34 +0700

    update docker compose

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,11 +1,13 @@
 import "./globals.css";
 import {GoogleOAuthProvider} from "@react-oauth/google";
 
+const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || '415209795379-fdvdvu59mam6itn486t81u150f8egc76.apps.googleusercontent.com';
+
 export default function RootLayout({ children }: { children: React.ReactNode; }) {
     return (
         <html lang="vi">
             <body>
-                <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string}>
+                <GoogleOAuthProvider clientId={clientId}>
                     {children}
                 </GoogleOAuthProvider>
             </body>
